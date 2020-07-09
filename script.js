9bcfed9807e2a236fa58eb93903156daf3b90eb6
Vincenzo <megnavincenzo97@gmail.com>
2020-07-09 17:25:17 +0200

    Nova function, ex finito

--- script.js
+++ script.js
@@ -15,7 +15,25 @@ for (var i = 0; i < 5; i++) {
 alert('Hai 30 secondi per tenere a mente questi numeri--> ' + arrayRand);
 console.log(arrayRand);
 
+// L'utente tramite i prompt deve inserire i numeri elencati prima
+setTimeout(insertNumber, 30000);
+
 // Function
+function insertNumber() {
+  var arrayUt = [];
+  for (var i = 0; i < 5; i++) {
+    var numUt = parseInt(prompt('Inserisci un numero.'));
+    for (var j = 0; j < arrayRand.length; j++) {
+      if(arrayRand[j] == numUt){
+        arrayUt.push(numUt);
+      }
+    }
+    console.log(numUt);
+  }
+  alert(' Hai indovinato ' + arrayUt.length + ' numeri' +
+  '\n Questi sono i numeri indovinati: ' + arrayUt);
+}
+
 function getRandom (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
